ef7e324981d3cd688a274433eca2b0947e8adf74
shobana0525 <shobanakarunanethi@gmail.com>
2025-09-21 20:06:21 +0530

    server.js get edited

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,10 @@ app.use("/api/todos", todoRoutes);
 // ✅ Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+  app.get("/:all(*)", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+});
+
 }
 
 
